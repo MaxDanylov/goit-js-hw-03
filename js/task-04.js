95c6a7618c2f3adf580d35661692faefff6d6e3e
Maksym Danylov <maxdanylov@gmail.com>
2020-08-10 22:49:46 +0300

    completed task-04

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,27 @@
 'use strict';
+const countTotalSalary = function (employees) {
+  const salaryes = Object.values(employees);
+  let total = 0;
+
+  for (const salary of salaryes) {
+    total += salary;
+  }
+
+  return total;
+};
+
+console.log(countTotalSalary({}));
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+);
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+);
