9b24b9f3244d9dd506fe8b508a94f7910a474da0
Maksym Danylov <maxdanylov@gmail.com>
2020-08-10 22:39:49 +0300

    completed task-03

--- js/task-03.js
+++ js/task-03.js
@@ -1,1 +1,39 @@
 'use strict';
+const findBestEmployee = function (employees) {
+  const names = Object.keys(employees);
+  let answer;
+  let name;
+  let bestResult = 0;
+
+  for (name of names) {
+    if (employees[name] > bestResult) {
+      bestResult = employees[name];
+      answer = `${name}:${bestResult}`;
+    }
+  }
+  return answer;
+};
+
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  }),
+);
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  }),
+);
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }),
+);
